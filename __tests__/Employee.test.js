61220505fac824d56759f027d3c35d19dbf1bd14
Ashley Hodge <ashhodge@comcast.net>
2022-02-27 17:47:47 -0500

    add functions to employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,3 +7,21 @@ test('Create a new employee object', () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 }); 
+
+test('get name as an object', () => {
+    const employee = new Employee('Ashley', 1, 'ashhodge@comcast.net');
+
+    expect(employee.getName()).toHaveProperty('name');
+});
+
+test('get id as an object', () => {
+    const employee = new Employee('Ashley', 1, 'ashhodge@comcast.net');
+
+    expect(employee.getId()).toHaveProperty('id');
+});
+
+test('get email as an object', () => {
+    const employee = new Employee('Ashley', 1, 'ashhodge@comcast.net');
+
+    expect(employee.getEmail()).toHaveProperty('email');
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,10 +1,15 @@
 const Manager = require('../lib/Manager');
+const Employee = require('../lib/Employee');
+
+jest.mock('../lib/Employee');
 
 test('Create a new manager object', () => {
     const manager = new Manager('Ashley', 1, 'ashhodge@comcast.net', 12);
 
-    expect(manager.name).toEqual(expect.any(String));
-    expect(manager.id).toEqual(expect.any(Number));
-    expect(manager.email).toEqual(expect.any(String));
     expect(manager.office).toEqual(expect.any(Number));
+    expect(manager.info).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+        
+    );
 }); 
+
