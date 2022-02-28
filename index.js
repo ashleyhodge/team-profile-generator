c6f86403a296b4508fad8a4c20a5b3f6d4c68ab0
Ashley Hodge <ashhodge@comcast.net>
2022-02-28 15:28:47 -0500

    add html

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-const generateHTML = require('./src/generateHTML');
+const generateHTML = require('./src/page-template');
 
 const teamArray = [];
 
--- src/page-template.js
+++ src/page-template.js
@@ -6,7 +6,7 @@ const genManager = function(manager) {
                 <h2>${manager.name}</h3>
                 <h3> Manager </h3>
             </div>
-            <div class= "card-body"
+            <div class= "card-body">
                 <h4 class= "id"> ID: ${manager.id}</h4>
                 <h4 class= "email"> Email Address: <a href= "mailto:${manager.email}"> ${manager.email} </h4>
             </div>
@@ -21,12 +21,12 @@ const genEngineer = function(engineer) {
         <div class= "card h-100">
             <div class= "card-header">
                 <h2>${engineer.name}</h3>
-                <h3> Manager </h3>
+                <h3> Engineer </h3>
             </div>
-            <div class= "card-body"
+            <div class= "card-body">
                 <h4 class= "id"> ID: ${engineer.id}</h4>
                 <h4 class= "email"> Email Address: <a href= "mailto:${engineer.email}"> ${engineer.email} </h4>
-                <h4 class+ "github"> GitHub Account: <a href="https://github.com/${engineer.github}"> ${engineer.github} </a></h4>
+                <h4 class= "github"> GitHub Account: <a href="https://github.com/${engineer.github}"> ${engineer.github} </a></h4>
             </div>
         </div>
     </div>
@@ -39,9 +39,9 @@ const genIntern = function(intern) {
         <div class= "card h-100">
             <div class= "card-header">
                 <h2>${intern.name}</h3>
-                <h3> Manager </h3>
+                <h3> Intern </h3>
             </div>
-            <div class= "card-body"
+            <div class= "card-body">
                 <h4 class= "id"> ID: ${intern.id} </h4>
                 <h4 class= "email"> Email Address: <a href= "mailto:${intern.email}"> ${intern.email} </h4>
                 <h4 class= "school"> School: ${intern.school} </h4>
@@ -51,26 +51,74 @@ const genIntern = function(intern) {
     `
 }
 
-const generatePage = function() {
-    return `
-        <!DOCTYPE html>
+generateHTML = (data) => {
+    profileArray = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole(); 
+
+
+        if (role === 'Manager') {
+            const managerCard = genManager(employee);
+
+            profileArray.push(managerCard);
+        }
+
+        if (role === 'Engineer') {
+            const engineerCard = genEngineer(employee);
+
+            profileArray.push(engineerCard);
+        }
+ 
+        if (role === 'Intern') {
+            const internCard = genIntern(employee);
+
+            profileArray.push(internCard);
+        }
+    }
+
+    const employeeCards = profileArray.join('')
+
+    const generateTeam = generatePage(employeeCards); 
+    return generateTeam;
+}
+
+const generatePage = function(employeeCards) {   
+    return`
+    <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Team Profile</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <header>
+            <nav class="navbar" id="navbar">
+                <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center" id="team-cards">
+                    <!--Team Cards-->
+                    ${employeeCards}
+                </div>
+            </div>
+        </main>
         
     </body>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     </html>
-    `
-}
+`};
 
 // export to index
 module.exports = generateHTML; 
